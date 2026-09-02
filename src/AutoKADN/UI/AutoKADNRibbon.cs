e8f7eab84e5fa5cf169ac624927bea15c24e644b
Karretdn <karretdn@gmail.com>
2026-09-02 00:36:37 -0500

    Improve AutoKADN Ribbon button depth and icons

--- src/AutoKADN/UI/AutoKADNRibbon.cs
+++ src/AutoKADN/UI/AutoKADNRibbon.cs
@@ -1,4 +1,5 @@
 using System.Windows.Input;
+using System.Windows.Media;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.Windows;
 
@@ -61,8 +62,8 @@ public sealed class AutoKADNRibbon
             Source = panelSource
         };
 
-        panelSource.Items.Add(CreateButton("NOMENK", "Nomenclatura", "NOMENK"));
-        panelSource.Items.Add(CreateButton("LIMIK", "Límites", "LIMIK"));
+        panelSource.Items.Add(CreateButton("NOMENK", "Nomenclatura", "NOMENK", CreateNomenclaturaIcon()));
+        panelSource.Items.Add(CreateButton("LIMIK", "Límites", "LIMIK", CreateLimiteIcon()));
 
         tab.Panels.Add(panel);
         ComponentManager.Ribbon.Tabs.Add(tab);
@@ -70,17 +71,50 @@ public sealed class AutoKADNRibbon
         _tab = tab;
     }
 
-    private static RibbonButton CreateButton(string id, string text, string command)
+    private static RibbonButton CreateButton(string id, string text, string command, ImageSource icon)
     {
         return new RibbonButton
         {
             Id = $"AutoKADN_{id}",
             Text = text,
             ShowText = true,
+            ShowImage = true,
             Orientation = System.Windows.Controls.Orientation.Vertical,
             Size = RibbonItemSize.Large,
+            LargeImage = icon,
             CommandHandler = new RibbonCommandHandler(command)
         };
+    }
+
+    private static DrawingImage CreateNomenclaturaIcon()
+    {
+        var group = new DrawingGroup();
+        using (DrawingContext dc = group.Open())
+        {
+            var pen = new Pen(Brushes.White, 2.2);
+            dc.DrawRoundedRectangle(null, pen, new System.Windows.Rect(4, 5, 32, 28), 4, 4);
+            dc.DrawLine(pen, new System.Windows.Point(9, 15), new System.Windows.Point(31, 15));
+            dc.DrawLine(pen, new System.Windows.Point(9, 22), new System.Windows.Point(26, 22));
+            dc.DrawEllipse(Brushes.White, null, new System.Windows.Point(30, 29), 3.2, 3.2);
+        }
+
+        return new DrawingImage(group);
+    }
+
+    private static DrawingImage CreateLimiteIcon()
+    {
+        var group = new DrawingGroup();
+        using (DrawingContext dc = group.Open())
+        {
+            var pen = new Pen(Brushes.White, 2.5);
+            dc.DrawLine(pen, new System.Windows.Point(5, 32), new System.Windows.Point(35, 7));
+            dc.DrawLine(pen, new System.Windows.Point(7, 12), new System.Windows.Point(34, 35));
+            dc.DrawEllipse(null, pen, new System.Windows.Point(20, 20), 4.5, 4.5);
+            dc.DrawLine(pen, new System.Windows.Point(20, 3), new System.Windows.Point(20, 10));
+            dc.DrawLine(pen, new System.Windows.Point(20, 30), new System.Windows.Point(20, 37));
+        }
+
+        return new DrawingImage(group);
     }
 
     private sealed class RibbonCommandHandler : ICommand
